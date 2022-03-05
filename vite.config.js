a6be7b4b7fecb1d8fa9a4d0189681f2ae88cb681
markthree <1801982702@qq.com>
2022-03-05 14:31:32 +0800

    chore(examples): add vue-project

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "path"
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -5,7 +6,7 @@ export default defineConfig({
         lib: {
             name: 'index',
             fileName: format => `index.${format}.js`,
-            entry: path.resolve(__dirname, 'src/index.ts')
+            entry: resolve(__dirname, 'src/index.js')
         },
         rollupOptions: {
             external: ['vue']
