20fef609ad18438fd4df86c37c09c0c856407162
markthree <1801982702@qq.com>
2022-03-05 20:44:45 +0800

    feat(shared): add useVModel, useVModels

--- src/shared/composables.js
+++ src/shared/composables.js
@@ -2,7 +2,13 @@
  * @description 自定义的基础 composition-api
  */
 
-import { unref } from 'vue'
+import {
+	ref,
+	watch,
+	unref,
+	computed,
+	getCurrentInstance
+} from 'vue'
 
 /**
  * 创建解 ref 函数
@@ -18,4 +24,80 @@ export const createUnrefFn = fn => {
 			args.map(v => unref(v))
 		)
 	}
+}
+
+/**
+ * 使用 v-model
+ * @template T
+ * @param {import('vue').Prop<T>} props 属性
+ * @param {string} key 键值
+ * @param {any} emit 发射
+ * @param {{ eventName: string, deep: boolean, passive: false }} options
+ * @returns {import('vue').ComputedRef<R>}
+ */
+export const useVModel = (
+	props,
+	key = 'modelValue',
+	emit,
+	options = {}
+) => {
+	const {
+		eventName,
+		deep = false,
+		passive = false
+	} = options
+
+	const vm = getCurrentInstance()
+
+	// 获取 emit
+	const _emit = emit || vm?.emit || vm?.$emit?.bind(vm)
+
+	// 获取事件名
+	let event = eventName || `update:${key}`
+
+	if (passive) {
+		const proxy = ref(props[key])
+
+		watch(
+			() => props[key],
+			v => (proxy.value = v)
+		)
+
+		watch(
+			proxy,
+			v => {
+				if (v !== props[key] || deep) _emit(event, v)
+			},
+			{
+				deep
+			}
+		)
+
+		return proxy
+	} else {
+		return computed({
+			get() {
+				return props[key]
+			},
+			set(value) {
+				_emit(event, value)
+			}
+		})
+	}
+}
+
+/**
+ * 使用多个 v-model
+ * @template T, R
+ * @param {import('vue').Prop<T>} props 属性
+ * @param {any} emit 发射
+ * @param {{ eventName: string, deep: boolean, passive: false }} options
+ * @returns {Record<string, import('vue').ComputedRef<R>>}
+ */
+export const useVModels = (props, emit, options = {}) => {
+	const ret = {}
+	for (const key in props) {
+		ret[key] = useVModel(props, key, emit, options)
+	}
+	return ret
 }
